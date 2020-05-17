1912ab57ab1e2be4d815dff01e56110eac5095a0
Kostya <Kpstya@mail.ru>
2020-05-17 19:49:47 +0300

    rm all json files

--- js/test.js
+++ js/test.js
@@ -1,2 +1,21 @@
-const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+const API = 'https://raw.githubusercontent.com/Konstantin108/project-Vue-test/master/approveReview.json';
 
+class Quiz {
+    constructor(api, container = ".quiz") {
+        this._fetch(api);
+    }
+
+    _fetch(api) {
+        return fetch(api)
+            .then((response) => response.json())
+            .then((response) => {
+                console.log(api.contents)
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+}
+
+new Quiz(API);
